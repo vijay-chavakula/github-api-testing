daf0a50137cf1eca0671bb88541c81f2c7390a83
karthik divi <kdivi@reputation.com>
2017-02-13 05:14:28 +0530

    Update 1.groovy

--- users/karthikdivi/scripts/1.groovy
+++ users/karthikdivi/scripts/1.groovy
@@ -2,7 +2,7 @@ hit 'http://lumtest.com/myip.json'
 json(response){
     // parsing
     def ip = text '$.ip'
-    if(ip){
+    if(!ip){
         result.failed = true
         result.message = 'lumtest is not giving IP'
     }
